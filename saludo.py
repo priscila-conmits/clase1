8152d4db1aefb94ae512598a144a6570a21957be
priscila-conmits <dominguezpriscila2005@gmail.com>
2025-05-05 22:04:22 -0300

    hola

--- saludo.py
+++ saludo.py
@@ -1,3 +1,5 @@
 nombre = input("¿Cómo te llamás? ")
-print(f"¡Hola, {nombre}! Bienvenido/a a Python.")
+print(f"¡Hola, {nombre}! Bienvenido/a a Python. esto es una prueba")
 ##Comentario
+
+print("Este es mi primer script con control de versiones.")
---
+++ script.py
@@ -0,0 +1,1 @@
+print("Este es mi primer script con control de versiones.")
